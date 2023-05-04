79a301cd55c06984a338a6260748029614a80f33
AdrianValentinCiu <111737211+AdrianValentinCiu@users.noreply.github.com>
2023-05-04 09:47:02 +0300

    upd

--- front_end/tech_blog_frontend/src/pages/Home.js
+++ front_end/tech_blog_frontend/src/pages/Home.js
@@ -4,17 +4,21 @@ import axios from 'axios';
 function Home() {
   const [topics, setTopics] = useState([]);
 
+  const getTopics = () => {
+    axios
+    .get("http://localhost:8080/api/v1/topic/topics")
+    .then((response) => {
+      setTopics(response.data);
+      console.log(response.data);
+    })
+    .catch((err) => {
+       console.log(err);
+    });
+  }
+
   useEffect(() => {
-     axios
-        .get("http://localhost:8080/api/v1/topic/topics")
-        .then((response) => {
-          setTopics(response.data);
-          console.log(response.data);
-        })
-        .catch((err) => {
-           console.log(err);
-        });
-  }, []);
+    getTopics()
+  }, [])
 
   return (
    <div>
