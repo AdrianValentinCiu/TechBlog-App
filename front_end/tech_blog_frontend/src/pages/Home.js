995285210b00c9bb1f560a16bb7373c0b3a5d090
AdrianValentinCiu <111737211+AdrianValentinCiu@users.noreply.github.com>
2023-05-06 11:51:05 +0300

    upd frontend

--- front_end/tech_blog_frontend/src/pages/Home.js
+++ front_end/tech_blog_frontend/src/pages/Home.js
@@ -5,20 +5,7 @@ import TopicMessages from "./TopicMessages"
 
 function Home() {
   const [topics, setTopics] = useState([]);
-  const [topicMessages, setTopicMessages] = useState([]);
-
-  function getTopicMessages(event, topicId){
-    axios
-    .get(`http://localhost:8080/api/v1/topic/topic-messages/${topicId}`)
-    .then((response) => {
-      setTopicMessages(response.data);
-      console.log(response.data);
-    })
-    .catch((err) => {
-       console.log(err);
-    });
-  }
-
+  
   const getTopics = () => {
     axios
     .get("http://localhost:8080/api/v1/topic/topics")
@@ -37,14 +24,18 @@ function Home() {
 //<a href = {`http://localhost:3000/`} onClick={(event) => getTopicMessages(event, topic.idTopic)} > 
 //<Link to={`/topic_messages/${topic.idTopic}`} state={{ topic: topic }}> {topic.topicTitle} </Link>
   return (
-   <div>
+   <div className="topic-messages-container">
       <ul>
         {topics.map(topic => (
           <li key={topic.idTopic}>
-            <a href = {`http://localhost:3000/`} onClick={(event) => getTopicMessages(event, topic.idTopic)} > 
-            {topic.topicTitle}
-            </a>
-            {'@'} {topic.fullName}
+            <div className="post">
+                  <div className="postTextContainer">
+                      <Link to="/topic_messages" state={topic}>
+                        {topic.topicTitle}
+                      </Link>
+                      {'@'} {topic.fullName}
+                  </div>
+            </div>
           </li>
         ))}
       </ul>
